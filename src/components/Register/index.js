2bbb4529fb9a793c4722be7ad78c8b2338364b20
Abner Qian <LargeBagel@gmail.com>
2019-06-26 11:47:06 -0700

    Removing unused import

--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -3,7 +3,6 @@ import { Mutation } from 'react-apollo';
 import { gql } from 'apollo-boost';
 import PropTypes from 'prop-types';
 import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
 import { Link as RouterLink } from 'react-router-dom';
 import Form from './Form';
 

b8bb7ba42152263f4548dc5eb1b23371e11e9e2b
Abner Qian <LargeBagel@gmail.com>
2019-07-25 12:45:36 -0700

    Refactor register page's form

--- src/components/Register/Form.js
+++ src/components/Register/Form.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import CloseIcon from '@material-ui/icons/Close';
 import { makeStyles } from '@material-ui/core/styles';
-import { useSnackbar } from 'notistack';
-import { Formik, Field, Form } from 'formik';
+import { Field, Form } from 'formik';
 import { TextField } from 'formik-material-ui';
-import _ from 'lodash';
-
-import RegisterSchema from './Schema';
 
 const useStyles = makeStyles(theme => ({
   form: {
@@ -22,115 +16,59 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const RegisterForm = ({
-  registerMutation,
-  onSuccess,
+  isSubmitting,
 }) => {
   const classes = useStyles();
 
-  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
-  const handleError = (message) => {
-    const action = key => (
-      <IconButton
-        key="close"
-        aria-label="Close"
-        color="inherit"
-        onClick={() => { closeSnackbar(key); }}
-      >
-        <CloseIcon />
-      </IconButton>
-    );
-    enqueueSnackbar(message, {
-      action,
-      variant: 'error',
-    });
-  };
-
   return (
-    <Formik
-      initialValues={{
-        login: '',
-        email: '',
-        password: '',
-        passwordConfirmation: '',
-      }}
-      validationSchema={RegisterSchema}
-      onSubmit={async (variables, { setSubmitting, setFieldError }) => {
-        let response;
+    <Form className={classes.form}>
+      <Field
+        label="Login"
+        name="login"
+        component={TextField}
+        required
+      />
 
-        try {
-          response = await registerMutation({ variables });
-        } catch (error) {
-          handleError(error.message);
-          return;
-        } finally {
-          setSubmitting(false);
-        }
+      <Field
+        label="Email"
+        type="email"
+        name="email"
+        component={TextField}
+        required
+      />
 
-        const { data: { register: { errors } } } = response;
+      <Field
+        label="Password"
+        type="password"
+        name="password"
+        component={TextField}
+        required
+      />
 
-        if (_.isEmpty(errors)) {
-          onSuccess();
-        } else {
-          _.each(errors, ({ path, message }) => {
-            setFieldError(path[1], message);
-          });
-        }
-      }}
-    >
-      {({
-        isSubmitting,
-      }) => (
-        <Form className={classes.form}>
-          <Field
-            label="Login"
-            name="login"
-            component={TextField}
-            required
-          />
+      <Field
+        label="Password Confirmation"
+        type="password"
+        name="passwordConfirmation"
+        component={TextField}
+        required
+      />
 
-          <Field
-            label="Email"
-            type="email"
-            name="email"
-            component={TextField}
-            required
-          />
-
-          <Field
-            label="Password"
-            type="password"
-            name="password"
-            component={TextField}
-            required
-          />
-
-          <Field
-            label="Password Confirmation"
-            type="password"
-            name="passwordConfirmation"
-            component={TextField}
-            required
-          />
-
-          <Button
-            variant="contained"
-            color="primary"
-            disabled={isSubmitting}
-            type="submit"
-            className={classes.submit}
-            fullWidth
-          >
-          Register
-          </Button>
-        </Form>
-      )}
-    </Formik>
+      <Button
+        variant="contained"
+        color="primary"
+        disabled={isSubmitting}
+        type="submit"
+        className={classes.submit}
+        fullWidth
+      >
+        Register
+      </Button>
+    </Form>
   );
 };
 
 RegisterForm.propTypes = {
-  registerMutation: PropTypes.func.isRequired,
-  onSuccess: PropTypes.func.isRequired,
+  isSubmitting: PropTypes.bool.isRequired,
 };
 
 export default RegisterForm;
--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Mutation } from 'react-apollo';
 import { gql } from 'apollo-boost';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import { useSnackbar } from 'notistack';
+import { Formik } from 'formik';
+import _ from 'lodash';
 
 import Link from 'components/Link';
+
+import Schema from './Schema';
 import Form from './Form';
 
 const registerMutation = gql`
@@ -27,20 +34,74 @@ const registerMutation = gql`
   }
 `;
 
-const Register = ({ onSuccess }) => (
-  <Mutation mutation={registerMutation}>
-    {
+const Register = ({ onSuccess }) => {
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+  const handleError = (message) => {
+    const action = key => (
+      <IconButton
+        key="close"
+        aria-label="Close"
+        color="inherit"
+        onClick={() => { closeSnackbar(key); }}
+      >
+        <CloseIcon />
+      </IconButton>
+    );
+    enqueueSnackbar(message, {
+      action,
+      variant: 'error',
+    });
+  };
+
+  return (
+    <Mutation mutation={registerMutation}>
+      {
       register => (
         <>
-          <Form registerMutation={register} onSuccess={onSuccess} />
+          <Formik
+            initialValues={{
+              login: '',
+              email: '',
+              password: '',
+              passwordConfirmation: '',
+            }}
+            validationSchema={Schema}
+            onSubmit={async (variables, { setSubmitting, setFieldError }) => {
+              let response;
+
+              try {
+                response = await register({ variables });
+              } catch (error) {
+                handleError(error.message);
+                return;
+              } finally {
+                setSubmitting(false);
+              }
+
+              const { data: { register: { errors } } } = response;
+
+              if (_.isEmpty(errors)) {
+                onSuccess();
+              } else {
+                _.each(errors, ({ path, message }) => {
+                  setFieldError(path[1], message);
+                });
+              }
+            }}
+          >
+            {({ isSubmitting }) => (
+              <Form isSubmitting={isSubmitting} />
+            )}
+          </Formik>
           <Link to="/login" variant="body2">
               Already have an account?
           </Link>
         </>
       )
     }
-  </Mutation>
-);
+    </Mutation>
+  );
+};
 
 Register.propTypes = {
   onSuccess: PropTypes.func.isRequired,

0cdf542dd251206aea928733e2f02166792bb568
Abner Qian <LargeBagel@gmail.com>
2020-03-07 17:06:22 -0800

    Using FormikMutation component for Login

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import { useMutation } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
 import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
-import { Formik } from 'formik';
 import _ from 'lodash';
 
+import FormikMutation from 'components/FormikMutation';
 import Link from 'components/Link';
-import useSnackbar from 'utils/snackbar';
 
 import LoginSchema from './Schema';
 import Form from './Form';
@@ -30,62 +28,47 @@ const loginMutation = gql`
   }
 `;
 
-const Login = ({ onLogin }) => {
-  const showSnackbar = useSnackbar();
-  const [login] = useMutation(loginMutation);
+const Login = ({ onLogin }) => (
+  <>
+    <FormikMutation
+      mutation={loginMutation}
+      onMutationSuccess={(response, { setFieldError }) => {
+        const { data: { login: { token, errors } } } = response;
 
-  return (
-    <>
-      <Formik
-        initialValues={{
-          login: '',
-          password: '',
-          rememberMe: false,
-        }}
-        validationSchema={LoginSchema}
-        onSubmit={async (variables, { setSubmitting, setFieldError }) => {
-          let response;
-
-          try {
-            response = await login({ variables });
-          } catch (error) {
-            showSnackbar(error.message, 'error');
-            return;
-          } finally {
-            setSubmitting(false);
-          }
-
-          const { data: { login: { token, errors } } } = response;
-
-          if (token) {
-            onLogin(token);
-          } else {
-            setFieldError('general', _.map(errors, 'message'));
-          }
-        }}
-      >
-        {({
-          isSubmitting,
-          errors,
-        }) => (
-          <Form isSubmitting={isSubmitting} errors={errors} />
-        )}
-      </Formik>
-      <Grid container>
-        <Grid item xs>
-          <Link to="/" variant="body2">
-                  Forgot password?
-          </Link>
-        </Grid>
-        <Grid item>
-          <Link to="/register" variant="body2">
-                  Don&apos;t have an account? Sign Up
-          </Link>
-        </Grid>
+        if (token) {
+          onLogin(token);
+        } else {
+          setFieldError('general', _.map(errors, 'message'));
+        }
+      }}
+      initialValues={{
+        login: '',
+        password: '',
+        rememberMe: false,
+      }}
+      validationSchema={LoginSchema}
+    >
+      {({
+        isSubmitting,
+        errors,
+      }) => (
+        <Form isSubmitting={isSubmitting} errors={errors} />
+      )}
+    </FormikMutation>
+    <Grid container>
+      <Grid item xs>
+        <Link to="/" variant="body2">
+          Forgot password?
+        </Link>
       </Grid>
-    </>
-  );
-};
+      <Grid item>
+        <Link to="/register" variant="body2">
+          Don&apos;t have an account? Sign Up
+        </Link>
+      </Grid>
+    </Grid>
+  </>
+);
 
 Login.propTypes = {
   onLogin: PropTypes.func,
